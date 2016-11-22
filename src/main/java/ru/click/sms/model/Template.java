abac6b1287d138d1149b5bffde99d4496e7282b7
Michael <shamanoff@gmail.com>
2016-11-22 20:51:12 +0200

    добавлена частичная реализация сервисов СМС

---
+++ src/main/java/ru/click/sms/model/Template.java
@@ -0,0 +1,60 @@
+package ru.click.sms.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * ДТО шаблона с СМС собщениями
+ */
+
+@Entity(name = "templates")
+public class Template {
+
+    public static Template of(String text, boolean hasParams) {
+        return new Template().setText(text).setHasParams(hasParams);
+    }
+
+    /**
+     * Идентификатор шаблона
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    /**
+     * Текст шаблона
+     */
+    private String text;
+    /**
+     * Имеет ли шаблон парамметры
+     */
+    private boolean hasParams;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Template setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Template setText(String text) {
+        this.text = text;
+        return this;
+    }
+
+    public boolean isHasParams() {
+        return hasParams;
+    }
+
+    public Template setHasParams(boolean hasParams) {
+        this.hasParams = hasParams;
+        return this;
+    }
+}
---
+++ src/main/java/ru/click/sms/service/TemplateServiceImpl.java
@@ -0,0 +1,55 @@
+package ru.click.sms.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.click.sms.TemplateService;
+import ru.click.sms.model.Template;
+import ru.click.sms.repository.TemplateRepository;
+
+import static org.springframework.util.Assert.notNull;
+
+/**
+ * Сервис для управления шаблонами СМС
+ */
+@Service
+public class TemplateServiceImpl implements TemplateService {
+
+    @Autowired
+    private TemplateRepository tempRep;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Integer addTemplate(String templateText) {
+        return addTemplate(templateText, false);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Integer addTemplate(String templateText, boolean hasParams) {
+        Template template = Template.of(templateText, hasParams);
+        Integer templateId = tempRep.save(template).getId();
+        notNull(templateId, "идентификатор шаблона не может быть пустым после сохранения в базу");
+        return templateId;
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean deleteTemplate(Integer templateId) {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean updateTemplate(Integer templateId, String templateText) {
+        return false;
+    }
+}
